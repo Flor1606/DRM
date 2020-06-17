27b2cc71727148531c7f34d07a7faaade0dd0701
Flor1606 <67064835+Flor1606@users.noreply.github.com>
2020-06-17 17:50:30 +0200

    Update __init__.py

--- DRM/__init__.py
+++ DRM/__init__.py
@@ -1,10 +1,10 @@
 from gym.envs.registration import register
 
 register(
-    id='foo-v0',
+    id='DRM-v0',
     entry_point='gym_foo.envs:FooEnv',
 )
 register(
-    id='foo-extrahard-v0',
-    entry_point='gym_foo.envs:FooExtraHardEnv',
+    id='DRM-extrahard-v0',
+    entry_point='gym_DRM.envs:DRMExtraHardEnv',
 )
